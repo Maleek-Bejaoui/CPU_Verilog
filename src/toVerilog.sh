5fe9ea0753cb0cace27fe0479a3b617ba1f3481d
Maleek <101218361+Maleek-Bejaoui@users.noreply.github.com>
2025-02-23 01:33:08 +0100

    Delete src/toVerilog.sh

--- src/toVerilog.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for file in *.vhd; do
-    if [ -f "$file" ]; then
-        base_name="${file%.vhd}"  # Supprime l'extension .vhd
-        ghdl synth --out=verilog "$file" > "$base_name.v"
-        echo "Generated: $base_name.v"
-    fi
-done
